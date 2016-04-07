34cf2121fef7325369bf454aca2fa203ca7a742e
Stephen McAuley <steviebiddles@gmail.com>
2016-04-07 18:24:40 +0100

    copy to clipboard

--- app/Resources/assets/js/site.js
+++ app/Resources/assets/js/site.js
@@ -1,2 +1,9 @@
-$(document).ready(function(){
-});
+(function () {
+    var clipboard = new Clipboard('.copy-trigger');
+
+    clipboard.on('success', function(e) {
+        //console.info('Text:', e.text);
+        $('.copy-trigger').text('Copied!');
+        e.clearSelection();
+    });
+})();
